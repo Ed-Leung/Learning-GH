7b32494d97e0323bb9f3549cb4276c3fb81f42fa
Edmond Leung <pixmation@mac.com>
2022-06-09 00:14:47 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Edmond Leung on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
